390548684f562a3917f211ebb38fd1c9863e0230
Daniel Chambers <code@danielchambe.rs>
2023-08-16 14:21:10 -0700

    added updated models for job board and listings for each company

--- application/periodic_tasks/models.py
+++ application/periodic_tasks/models.py
@@ -1,5 +1,6 @@
 import datetime
-from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, ForeignKey
+from sqlalchemy.orm import relationship
 from application.database import Base
 
 
@@ -7,29 +8,78 @@ class GreenhouseCompany(Base):
     __tablename__ = 'greenhouse_companies'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
-    timestamp_found = Column(TIMESTAMP, default=datetime.datetime.utcnow)
+    identifier = Column(String(75), unique=True)
+    company = Column(String(75))
+    website = Column(String(100))
+    found_date = Column(TIMESTAMP, server_default='NOW()')
     is_active = Column(Boolean, default=True)
-    jobs_available = Column(Integer)
+    total_jobs = Column(Integer, default=0)
+
+    jobs = relationship('GreenhouseJob', back_populates='company')
+
+
+class GreenhouseJob(Base):
+    __tablename__ = 'greenhouse_jobs'
+
+    id = Column(Integer, primary_key=True)
+    company_id = Column(Integer, ForeignKey('greenhouse_companies.id'))
+    title = Column(String(150))
+    location = Column(String(25))
+    job_id = Column(String(25))
+    listing_date = Column(TIMESTAMP)
+
+    company = relationship('GreenhouseCompany', back_populates='jobs')
 
 
 class LeverCompany(Base):
     __tablename__ = 'lever_companies'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
-    timestamp_found = Column(TIMESTAMP, default=datetime.datetime.utcnow)
+    identifier = Column(String(75), unique=True)
+    company = Column(String(75))
+    website = Column(String(100))
+    found_date = Column(TIMESTAMP, server_default='NOW()')
     is_active = Column(Boolean, default=True)
-    jobs_available = Column(Integer)
+    total_jobs = Column(Integer, default=0)
+
+    jobs = relationship('LeverJob', back_populates='company')
+
+
+class LeverJob(Base):
+    __tablename__ = 'lever_jobs'
+
+    id = Column(Integer, primary_key=True)
+    company_id = Column(Integer, ForeignKey('lever_companies.id'))
+    title = Column(String(150))
+    location = Column(String(25))
+    job_id = Column(String(25))
+    listing_date = Column(TIMESTAMP)
+
+    company = relationship('LeverCompany', back_populates='jobs')
 
 
 class MyWorkdayJobsCompany(Base):
-    __tablename__ = 'myworkdayjobs_companies'
+    __tablename__ = 'myworkdayjob_companies'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
-    region = Column(String(5))
-    uri = Column(String(50))
-    timestamp_found = Column(TIMESTAMP, default=datetime.datetime.utcnow)
+    identifier = Column(String(75), unique=True)
+    company = Column(String(75))
+    website = Column(String(100))
+    found_date = Column(TIMESTAMP, server_default='NOW()')
     is_active = Column(Boolean, default=True)
-    jobs_available = Column(Integer)
+    total_jobs = Column(Integer, default=0)
+
+    jobs = relationship('MyWorkdayJob', back_populates='company')
+
+
+class MyWorkdayJob(Base):
+    __tablename__ = 'myworkdayjob_jobs'
+
+    id = Column(Integer, primary_key=True)
+    company_id = Column(Integer, ForeignKey('myworkdayjob_companies.id'))
+    title = Column(String(150))
+    location = Column(String(25))
+    job_id = Column(String(25))
+    listing_date = Column(TIMESTAMP)
+
+    company = relationship('MyWorkdayJobsCompany', back_populates='jobs')
