5dcc5e5291dff32f10b4917dcc40d42368160a06
Daniel Chambers <code@danielchambe.rs>
2023-08-17 10:23:17 -0700

    added try catch to database function and new logic for task to collect jobs from various companies

--- application/periodic_tasks/main.py
+++ application/periodic_tasks/main.py
@@ -1,11 +1,18 @@
-import random
+import redis
+import logging
 from contextlib import contextmanager
 from celery import shared_task
 from sqlalchemy.exc import IntegrityError
 from application.database import SessionLocal
-from .models import GreenhouseCompany
+from .models import GreenhouseCompany, GreenhouseJob
 from .models import LeverCompany
-from .company_finder import company_names
+from .company_names import get_company_names
+from .company_jobs import get_greenhouse_jobs, parse_greenhouse_job
+
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 @contextmanager
@@ -13,10 +20,12 @@ def get_db():
     session = SessionLocal()
     try:
         yield session
+        session.commit()
     except IntegrityError:
-        # already exists
+        logger.warning("Integrity error occurred. Rolling back.")
         session.rollback()
-    except:
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
         session.rollback()
         raise
     finally:
@@ -24,7 +33,7 @@ def get_db():
 
 
 @shared_task
-def get_company_names(job_board, state, query):
+def company_names(job_board, state, query):
     states = {
         'AL': 'Alabama',
         'AK': 'Alaska',
@@ -88,7 +97,7 @@ def get_company_names(job_board, state, query):
         'jobvite': 'jobs.jobvite.com'
     }
     search_query = f'site:{job_boards[job_board]} {states[state]} OR {state} AND {query}'
-    names = company_names(job_board, search_query)
+    names = get_company_names(job_board, search_query)
 
     if len(names) > 0:
         for name in names:
@@ -100,3 +109,35 @@ def get_company_names(job_board, state, query):
             with get_db() as db:
                 db.add(new_company[job_board])
                 db.commit()
+
+
+@shared_task
+def greenhouse_jobs():
+    redis_client = redis.Redis(host='redis', port=6379, db=2)
+    page_size = 10  # Adjust page size if needed
+    page_number = 1
+
+    if redis_client.exists('greenhouse_company_page_number'):
+        page_number = int(redis_client.get('greenhouse_company_page_number'))
+
+    with get_db() as db:
+        offset = (page_number - 1) * page_size
+        companies = db.query(GreenhouseCompany).limit(
+            page_size).offset(offset).all()
+        redis_client.set('greenhouse_company_page_number', page_number + 1)
+
+    for company in companies:
+        logger.info(f"Fetching jobs for {company.identifier}")
+        listing = get_greenhouse_jobs(company.identifier)
+        jobs = listing['jobs']
+        total_jobs = listing['meta']['total']
+        logger.info(f"Total jobs: {total_jobs}")
+
+        if total_jobs > 0:
+            with get_db() as db:
+                for job in jobs:
+                    new_job_data = parse_greenhouse_job(company.id, job)
+                    new_job = GreenhouseJob(**new_job_data)
+                    db.add(new_job)
+                logger.info(
+                    f"Inserted {len(jobs)} jobs for {company.identifier}")
