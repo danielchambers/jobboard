30d7de6b2534d22e055fa05bd0f7495e70816cce
Daniel Chambers <code@danielchambe.rs>
2023-08-17 10:26:23 -0700

    changed the length of webite, title, location, job_id for data that exceeded the length of those fields

--- application/periodic_tasks/models.py
+++ application/periodic_tasks/models.py
@@ -10,7 +10,7 @@ class GreenhouseCompany(Base):
     id = Column(Integer, primary_key=True)
     identifier = Column(String(75), unique=True)
     company = Column(String(75))
-    website = Column(String(100))
+    website = Column(String(150))
     found_date = Column(TIMESTAMP, server_default='NOW()')
     is_active = Column(Boolean, default=True)
     total_jobs = Column(Integer, default=0)
@@ -23,9 +23,9 @@ class GreenhouseJob(Base):
 
     id = Column(Integer, primary_key=True)
     company_id = Column(Integer, ForeignKey('greenhouse_companies.id'))
-    title = Column(String(150))
-    location = Column(String(25))
-    job_id = Column(String(25))
+    title = Column(String(250))
+    location = Column(String(125))
+    job_id = Column(String(35))
     listing_date = Column(TIMESTAMP)
 
     company = relationship('GreenhouseCompany', back_populates='jobs')
@@ -50,9 +50,9 @@ class LeverJob(Base):
 
     id = Column(Integer, primary_key=True)
     company_id = Column(Integer, ForeignKey('lever_companies.id'))
-    title = Column(String(150))
-    location = Column(String(25))
-    job_id = Column(String(25))
+    title = Column(String(250))
+    location = Column(String(125))
+    job_id = Column(String(35))
     listing_date = Column(TIMESTAMP)
 
     company = relationship('LeverCompany', back_populates='jobs')
@@ -77,9 +77,9 @@ class MyWorkdayJob(Base):
 
     id = Column(Integer, primary_key=True)
     company_id = Column(Integer, ForeignKey('myworkdayjob_companies.id'))
-    title = Column(String(150))
-    location = Column(String(25))
-    job_id = Column(String(25))
+    title = Column(String(250))
+    location = Column(String(125))
+    job_id = Column(String(35))
     listing_date = Column(TIMESTAMP)
 
     company = relationship('MyWorkdayJobsCompany', back_populates='jobs')
