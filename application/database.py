34a79aff82b63406841ba0509e17788e5dca6670
Daniel Chambers <code@danielchambe.rs>
2023-08-21 14:55:02 -0700

    added database session context function from the task script into the database script allowing for better importing

--- application/database.py
+++ application/database.py
@@ -1,5 +1,7 @@
-import os
+import logging
+from contextlib import contextmanager
 from sqlalchemy import create_engine
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from application.config import settings
@@ -8,3 +10,25 @@ database_url = f'postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWO
 engine = create_engine(database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def database_session():
+    session = SessionLocal()
+    try:
+        yield session
+        session.commit()
+    except IntegrityError:
+        logger.warning("Integrity error occurred. Rolling back.")
+        session.rollback()
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
+database_context = contextmanager(database_session)
