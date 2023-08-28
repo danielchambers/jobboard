c4509bf897b72080d4ac09532142b73965f9dcc2
Daniel Chambers <code@danielchambe.rs>
2023-08-28 11:52:33 -0700

    moved google_search to its own file and the instantiation out of the parse function additionally updated docs and type hinting

--- application/periodic_tasks/task_utilities/company_finder.py
+++ application/periodic_tasks/task_utilities/company_finder.py
@@ -1,137 +1,90 @@
 import os
 import re
-import requests
 from typing import List, Dict, Union, Any, Optional
-
-
-def google_search(query: str, start_index: int = 1, num_results: int = 10) -> Optional[Dict[str, Any]]:
-    """
-    Perform a Google Custom Search API request and return the search results.
-
-    :param query: The search query.
-    :param start_index: The index of the first result to retrieve. Default is 1.
-    :param num_results: The number of results to retrieve. Default is 10.
-    :return: The search results as a dictionary or None if there was an error.
-    """
-    params = {
-        'key': os.getenv('GOOGLE_SEARCH_API_KEY'),
-        'cx': os.getenv('GOOGLE_SEARCH_ENGINE_ID'),
-        'q': query,
-        'start': start_index,
-        'num': num_results,
-        # 'dateRestrict': 'm6'
-    }
-
-    try:
-        response = requests.get(
-            'https://www.googleapis.com/customsearch/v1', params=params)
-        response.raise_for_status()
-        search_results = response.json()
-        return search_results
-    except requests.exceptions.HTTPError as err:
-        print(err.response.status_code)
 
 
 class CompanyFinder:
     """
-    A class for finding job listings from various company job boards.
-
-    Attributes:
-        job_board (str): The job board platform to search on.
-        unique_companies (set): A set to store unique company names.
-        companies (list): A list to store company information.
+    Find company information from job links on various platforms.
     """
 
-    def __init__(self, job_board: str) -> None:
+    def __init__(self, platform: str, platform_config: Dict[str, Dict[str, str]]) -> None:
         """
-        Initialize the CompanyFinder.
+        Initialize the CompanyFinder instance.
 
         Args:
-            job_board (str): The job board platform to search on.
+            platform: The selected job board.
+            platform_config: The configuration for the selected platform.
         """
-        self.job_board = job_board
+        self.platform = platform
+        self.platform_config = platform_config
         self.unique_companies = set()
         self.companies = []
 
-    def search(self, search_query: str) -> List[Dict[str, Union[str, str]]]:
+    def parse(self, links: List[str]) -> List[Dict[str, Union[str, str]]]:
         """
-        Search for companies based on the provided search query.
+        Parse job links to extract company information.
 
         Args:
-            search_query (str): The query to search for.
+            links: List of job links.
 
         Returns:
-            List[Dict[str, Union[str, str]]]: A list of dictionaries containing company information.
+            List of parsed company information.
         """
-        page_index = 1
-        page_results = 10
-        full_search_query = {
-            'greenhouse': f'site:boards.greenhouse.io {search_query}',
-            'lever': f'site:jobs.lever.co {search_query}',
-            'careerpuck': f'site:app.careerpuck.com/job-board/ {search_query}',
-            'ashbyhq': f'site:jobs.ashbyhq.com {search_query}',
-            'myworkdayjobs': f'site: myworkdayjobs.com {search_query}'
-        }
-        url_pattern = {
-            'greenhouse': r"boards\.greenhouse\.io\/(?P<company_name>\w+)",
-            'lever': r"jobs\.lever\.co\/(?P<company_name>\w+)",
-            'careerpuck': r"app\.careerpuck\.com\/job-board\/(?P<company_name>\w+)",
-            'ashbyhq': r"jobs\.ashbyhq\.com\/(?P<company_name>\w+)",
-            'myworkdayjobs': r'(?P<company_name>\w+)\.(?P<company_region>\w+)\.myworkdayjobs\.com\/(?:[a-z]{2}-[A-Z]{2}\/)?(?P<company_uri>\w[\w\-]*|(?!.*\/)([^\/?#]+))'
-        }
-        while True:
-            search_results = google_search(
-                full_search_query[self.job_board], page_index, page_results)
-            if search_results is None:
-                break
-            if 'nextPage' not in search_results['queries'].keys():
-                break
-            for item in search_results.get('items'):
-                match = re.search(
-                    url_pattern[self.job_board], item['link'])
-                if self.job_board == "myworkdayjobs":
-                    self.get_myworkdayjob(match)
+        if not self.platform_config:
+            raise ValueError(f"Invalid job board: {self.platform}")
+
+        for link in links:
+            match = re.search(self.platform_config['url_pattern'], link)
+            if match:
+                if self.platform == "myworkdayjobs":
+                    self._process_myworkdayjob_match(match)
                 else:
-                    self.get_company(match)
-            page_index += page_results
+                    self._process_company_match(
+                        match, self.platform_config['url_format'])
         return self.companies
 
-    def get_company(self, match: re.Match) -> None:
+    def _process_company_match(self, match: re.Match, url_format: str) -> None:
         """
-        Extract and store company information from the provided match.
+        Extract company information from a job link.
 
         Args:
-            match (re.Match): The regular expression match object.
+            match: A regular expression match object.
+            url_format: URL format for the company's API endpoint.
         """
-        if match:
+        try:
             company_name = match.group("company_name")
-            url = {
-                'greenhouse': f'https://boards-api.greenhouse.io/v1/boards/{company_name}/jobs/',
-                'lever': f'https://jobs.lever.co/v0/postings/{company_name}?mode=json',
-                'careerpuck': f'https://api.careerpuck.com/v1/public/job-boards/{company_name}',
-                'ashbyhq': 'https://jobs.ashbyhq.com/api/non-user-graphql?op=ApiJobBoardWithTeams'
-            }
             if company_name not in self.unique_companies:
+                company_url = url_format.format(company_name)
                 self.companies.append({
                     'name': company_name,
-                    'url': url[self.job_board],
-                    'platform': self.job_board
+                    'url': company_url,
+                    'platform': self.platform
                 })
-            self.unique_companies.add(company_name)
+                self.unique_companies.add(company_name)
+        except (AttributeError, IndexError, re.error) as err:
+            print(f"Error processing link: {err}")
 
-    def get_myworkdayjob(self, match: re.Match) -> None:
+    def _process_myworkdayjob_match(self, match: re.Match) -> None:
         """
-        Extract and store myworkdayjobs company information from the provided match.
+        Extract company information from a MyWorkDayJobs link.
 
         Args:
-            match (re.Match): The regular expression match object.
+            match: A regular expression match object.
         """
-        if match:
-            company_name = match.group("company_name")
-            if company_name not in self.unique_companies:
-                company_location = match.group("company_region")
-                company_uri = match.group("company_uri")
-                json_url = f'https://{company_name}.{company_location}.myworkdayjobs.com/wday/cxs/{company_name}/{company_uri}/jobs'
-                self.companies.append(
-                    {'name': company_name, 'url': json_url, 'platform': self.job_board})
-            self.unique_companies.add(company_name)
+        try:
+            if match:
+                company_name = match.group("company_name")
+                if company_name not in self.unique_companies:
+                    company_location = match.group("company_region")
+                    company_uri = match.group("company_uri")
+                    json_url = self.platform_config['url_format'].format(
+                        company_name=company_name,
+                        company_region=company_location,
+                        company_uri=company_uri
+                    )
+                    self.companies.append(
+                        {'name': company_name, 'url': json_url, 'platform': self.platform})
+                    self.unique_companies.add(company_name)
+        except (AttributeError, IndexError, re.error) as err:
+            print(f"Error processing MyWorkDayJobs link: {err}")
