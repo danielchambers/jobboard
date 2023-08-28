a514f681d2a49d8af0ca8d3aaf01e8d067082bdc
Daniel Chambers <code@danielchambe.rs>
2023-08-28 00:01:16 -0700

    added updated logic for collecting company names to celery task

--- application/periodic_tasks/main.py
+++ application/periodic_tasks/main.py
@@ -4,12 +4,60 @@ from sqlalchemy.exc import IntegrityError
 from application.database import database_context
 from application.models.jobs import Company
 from application.periodic_tasks.task_utilities.company_finder import CompanyFinder
+from application.periodic_tasks.task_utilities.google_search import GoogleSearch
 
 
 @shared_task
-def company_names(job_board, location, query):
-    cf = CompanyFinder(job_board)
-    companies = cf.search(f'{location} AND {query}')
+def company_names(job_platform, location, job_title):
+    MAX_SERACH_RESULTS = 91
+    PLATFORMS = {
+        'greenhouse': {
+            'search_query': 'site:boards.greenhouse.io {}',
+            'url_pattern': r"boards\.greenhouse\.io\/(?P<company_name>\w+)",
+            'url_format': 'https://boards-api.greenhouse.io/v1/boards/{}/jobs/',
+        },
+        'lever': {
+            'search_query': 'site:jobs.lever.co {}',
+            'url_pattern': r"jobs\.lever\.co\/(?P<company_name>\w+)",
+            'url_format': 'https://jobs.lever.co/v0/postings/{}/?mode=json',
+        },
+        'careerpuck': {
+            'search_query': 'site:app.careerpuck.com/job-board/ {}',
+            'url_pattern': r"app\.careerpuck\.com\/job-board\/(?P<company_name>\w+)",
+            'url_format': 'https://api.careerpuck.com/v1/public/job-boards/{}',
+        },
+        'ashbyhq': {
+            'search_query': 'site:jobs.ashbyhq.com {}',
+            'url_pattern': r"jobs\.ashbyhq\.com\/(?P<company_name>\w+)",
+            'url_format': 'https://jobs.ashbyhq.com/api/non-user-graphql?op=ApiJobBoardWithTeams',
+        },
+        'myworkdayjobs': {
+            'search_query': 'site:myworkdayjobs.com {}',
+            'url_pattern': r'(?P<company_name>\w+)\.(?P<company_region>\w+)\.myworkdayjobs\.com\/(?:[a-z]{2}-[A-Z]{2}\/)?(?P<company_uri>\w[\w\-]*|(?!.*\/)([^\/?#]+))',
+            'url_format': 'https://{company_name}.{company_region}.myworkdayjobs.com/wday/cxs/{company_name}/{company_uri}/jobs',
+        },
+    }
+
+    results = []
+    page_index = 1
+    page_results = 10
+    query = PLATFORMS.get(job_platform).get(
+        'search_query').format(f'{location} AND {job_title}')
+
+    while True:
+        search_results = GoogleSearch.search(query, page_index, page_results)
+        if page_index >= MAX_SERACH_RESULTS:
+            break
+        if search_results.get('items') is None:
+            break
+        items = search_results.get('items')
+        links = [item['link'] for item in items if items]
+        results.extend(links)
+        page_index += page_results
+
+    # print(results)
+    company_finder = CompanyFinder(job_platform, PLATFORMS.get(job_platform))
+    companies = company_finder.parse(results)
     for company in companies:
         print(company)
 
