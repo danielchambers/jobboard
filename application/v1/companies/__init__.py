9c763140f26b5af5367649117e83bec42f668843
Daniel Chambers <code@danielchambe.rs>
2023-09-01 19:34:27 -0700

    refactored company endpoints changed full text search to ilike since company names are al one word

--- application/v1/companies/__init__.py
+++ application/v1/companies/__init__.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Query
-from sqlalchemy.sql import func, text
-from sqlalchemy.orm import Query as SQLAlchemyQuery
+from sqlalchemy import func, or_
+from fastapi import APIRouter, Query, HTTPException
 from application.database import database_context
 from application.models.jobs import Company
+from application.v1.companies.utilities import company_to_dict
 
 companies_router = APIRouter(
     prefix="/v1/companies"
@@ -15,39 +15,36 @@ def return_all_company_names(
     page: int = Query(1, alias="page"),
     limit: int = Query(10, alias="limit")
 ):
-    if q:
-        return {'endpoint': f'/q={q}&page={page}&limit={limit}'}
-    else:
-        return {'endpoint': '/'}
-
-
-@companies_router.get('/{company_name}')
-def return_one_company_name(company_name):
     with database_context() as db:
-        return {'endpoint': f'{company_name}'}
+        query_base = db.query(Company)
+
+        if q:
+            filtered_query = query_base.filter(
+                or_(Company.company == q, Company.company.ilike(f"%{q}%")))
+        else:
+            filtered_query = query_base
+        offset = (page - 1) * limit if page > 0 else 0
+        records = filtered_query.offset(offset).limit(limit).all()
+        total_records = filtered_query.count()
+        return_data = {
+            'total': total_records,
+            'results': [company_to_dict(record) for record in records]
+        }
+
+        return return_data
 
 
-# @companies_router.get('/')
-# def return_search_company_names(
-#     q: str | None = None,
-#     page: int = Query(1, alias="page"),
-#     limit: int = Query(10, alias="limit")
-# ):
-#     print(page, limit)
-#     return {'endpoint': f'/q={q}'}
-    # with database_context() as db:
-    #     endpoint_results = {'q': q}
-    #     if q:
-    #         query: SQLAlchemyQuery = db.query(GreenhouseJob).filter(
-    #             func.to_tsvector('english', GreenhouseJob.title).op(
-    #                 '@@')(f"'{q}'")
-    #         )
-    #     else:
-    #         query: SQLAlchemyQuery = db.query(GreenhouseJob)
+@companies_router.get('/{company}')
+def return_one_company_name(company):
+    with database_context() as db:
+        records = db.query(Company).filter_by(company=company).all()
 
-    #     total_results = query.count()
-    #     results = query.offset((page - 1) * limit).limit(limit).all()
-    #     jobs = [{'title': result.title, 'location': result.location}
-    #             for result in results]
-    #     endpoint_results.update({'jobs': jobs, 'total': total_results})
-    # return endpoint_results
+        if not records:
+            raise HTTPException(status_code=404, detail="Company not found")
+
+        return_data = {
+            'total': len(records),
+            'results': [company_to_dict(record) for record in records]
+        }
+
+        return return_data
