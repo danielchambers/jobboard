4de41a8fdc7c30ca0ddeb0f96b2e960f107caac0
Daniel Chambers <code@danielchambe.rs>
2023-09-06 11:58:15 -0700

    changed model names so they down conflict also changed requests to asynchronous requests with aiohttp and asyncio to get more job listings faster

--- application/periodic_tasks/main.py
+++ application/periodic_tasks/main.py
@@ -1,10 +1,16 @@
+import json
 import redis
 import logging
+import asyncio
+import aiohttp
+import datetime
 from celery import shared_task, current_task
+from sqlalchemy import and_
 from application.database import database_context
-from application.models.jobs import Company
+from application.models.jobs import Company as Company_Model
+from application.models.jobs import Job as Job_Model
 from application.periodic_tasks.task_utilities.company_finder import CompanyFinder
-from application.periodic_tasks.task_utilities.company_jobs import Job
+from application.periodic_tasks.task_utilities.company_jobs import Job as Job_Finder
 from application.periodic_tasks.task_utilities.google_search import GoogleSearch
 
 import requests
@@ -63,31 +69,21 @@ def company_names(job_platform, location, job_title):
     company_finder = CompanyFinder(job_platform, PLATFORMS.get(job_platform))
     companies = company_finder.parse(results)
     with database_context() as db:
-        db.add_all([Company(**company) for company in companies])
+        db.add_all([Company_Model(**company) for company in companies])
 
 
 @shared_task
 def collect_jobs():
     last_page = int(redis_client.get('last_page') or 1)
-    page_size = 10
+    page_size = 50
     offset = (last_page - 1) * page_size
 
     with database_context() as db:
-        records = db.query(Company).filter_by(
-            platform='greenhouse').offset(offset).limit(page_size).all()
-
-        for record in records:
-            print(f'{record.company} -{record.platform} - {record.url}')
-            if record.platform == 'greenhouse':
-                response = requests.get(record.url)
-                total_jobs = response.json()['meta']['total']
-                print(total_jobs)
-                print('========================================')
-                jobs = response.json()['jobs']
-                for job in jobs:
-                    job = Job(record.company, job['id'], job['title'],
-                              job['location']['name'], job['updated_at'], job['absolute_url'])
-                    print(job.data())
+        records = db.query(Company_Model).filter(and_(
+            Company_Model.platform == 'greenhouse',
+            Company_Model.is_active == True)
+        ).offset(offset).limit(page_size).all()
+        asyncio.run(main(records, db))
 
     if len(records) < page_size:
         last_page = 1
@@ -96,3 +92,30 @@ def collect_jobs():
         current_task.apply_async(countdown=60)
 
     redis_client.set('last_page', last_page)
+
+
+async def main(records, db):
+    tasks = []
+    for record in records:
+        task = process_record(db, record)
+        # print(task)
+        tasks.append(task)
+    await asyncio.gather(*tasks)
+
+
+async def process_record(db, record):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(record.url) as response:
+            if response.status == 200:
+                job_listing = await response.json()
+                for job in job_listing['jobs']:
+                    job_finder = Job_Finder(job['id'], job['title'], job['location']['name'],
+                                            job['updated_at'], job['absolute_url'])
+                    job_data = job_finder.data()
+                    new_job = Job_Model(company_id=record.id, job_id=job_data['id'],
+                                        title=job_data['title'], country='Country',
+                                        state='State', city='City', is_remote=False,
+                                        is_hybrid=False, is_onsite=True, salary_low=80000.00,
+                                        salary_high=120000.00, keywords=['keyword1', 'keyword2'],
+                                        updated_at=datetime.datetime.now(), url=job_data['url'])
+                    db.merge(new_job)
