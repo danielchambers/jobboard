78eab53e8d29c4444b00f390d81f14bf439ad647
Daniel Chambers <code@danielchambe.rs>
2023-10-11 14:34:52 -0700

    added doc strings and changed save route to post and removed hardcoded values

--- application/users/__init__.py
+++ application/users/__init__.py
@@ -7,7 +7,11 @@ from application.models.user import (
     UserJobWatched,
     UserJobApplied
 )
-from application.users.schemas.authentication import UserLogin, UserRegistration
+from application.users.schemas.authentication import (
+    UserLogin,
+    UserRegistration,
+)
+from application.users.schemas.jobs import UserSaveJob
 from application.users.utilities.authentication import (
     create_access_token,
     authenticate_user,
@@ -89,14 +93,24 @@ async def login_user(user_data: UserLogin):
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@users_router.get("/jobs/save")
-async def secure_test(current_user: dict = Depends(authorize_user)):
+@users_router.post("/jobs/save")
+async def secure_test(payload: UserSaveJob, current_user: dict = Depends(authorize_user)):
+    """
+    Save a job for the current user.
+
+    Args:
+        payload (UserSaveJob): The payload containing the job information to be saved.
+        current_user (dict): The authenticated user's information.
+
+    Returns:
+        dict: A response message and the current user's information.
+    """
     user_id = current_user['id']
-    job_id = '317cf942-1fb9-4b77-8e76-342b2965adce'
-    platform = 'lever'
+
     with database_context() as db:
         existing_user = db.query(User).filter(User.id == user_id).first()
-        job = UserJobWatched(user_id=user_id, job_id=job_id, platform=platform)
+        job = UserJobWatched(
+            user_id=user_id, job_id=payload.job_id, platform=payload.platform)
         existing_user.jobs_watched.append(job)
         db.add(job)
     return {"message": "Job saved!", "user": current_user}
@@ -104,7 +118,17 @@ async def secure_test(current_user: dict = Depends(authorize_user)):
 
 @users_router.get("/jobs/saved")
 async def secure_test2(current_user: dict = Depends(authorize_user)):
+    """
+    Retrieve jobs saved by the current user.
+
+    Args:
+        current_user (dict): The authenticated user's information.
+
+    Returns:
+        dict: A list of saved jobs and the current user's information.
+    """
     user_id = current_user['id']
+
     with database_context() as db:
         existing_user = db.query(User).filter(User.id == user_id).first()
         jobs = [{'jobs': job.job_id, 'platform': job.platform}
